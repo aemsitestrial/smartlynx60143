c04ef29389925fad7ccee6debd98929eb3e70dc7
Madumitha M <2962517@tcs.com>
2026-09-23 14:56:10 +0530

    Changes in quotes code

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,20 +1,34 @@
 export default async function decorate(block) {
-  const [quotation, attribution] = [...block.children].map((c) => c.firstElementChild);
+  const quotation = block.children[0]?.firstElementChild;
+  const attribution = block.children[1]?.firstElementChild;
+  const layoutField = block.children[2]?.firstElementChild;
+
   const blockquote = document.createElement('blockquote');
-  // decorate quotation
+
+  let layout = 'default';
+
+  if (layoutField) {
+    layout = layoutField.textContent.trim().toLowerCase();
+  }
+
+  blockquote.classList.add(layout);
+
   quotation.className = 'quote-quotation';
   blockquote.append(quotation);
-  // decoration attribution
+
   if (attribution) {
     attribution.className = 'quote-attribution';
     blockquote.append(attribution);
+
     const ems = attribution.querySelectorAll('em');
+
     ems.forEach((em) => {
       const cite = document.createElement('cite');
       cite.innerHTML = em.innerHTML;
       em.replaceWith(cite);
     });
   }
+
   block.innerHTML = '';
   block.append(blockquote);
 }
